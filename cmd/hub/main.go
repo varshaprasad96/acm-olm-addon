6d1bb37d9bf741b151ef82c9069fbc09b86eb115
Frederic Giloux <fgiloux@redhat.com>
2023-03-01 13:43:53 +0100

    Changes to have the manager spawning agents with manifests using values from addondeploymentconfig
    Signed-off-by: Frederic Giloux <fgiloux@redhat.com>

--- cmd/hub/main.go
+++ cmd/hub/main.go
@@ -6,31 +6,20 @@ import (
 	"os"
 
 	restclient "k8s.io/client-go/rest"
+
 	"k8s.io/klog/v2"
 
 	"open-cluster-management.io/addon-framework/pkg/addonmanager"
-	addonv1alpha1client "open-cluster-management.io/api/client/addon/clientset/versioned"
 	"open-cluster-management.io/olm-addon/pkg/hub"
-)
-
-const (
-	addonName = "olm-addon"
 )
 
 //go:embed manifests
 var FS embed.FS
 
 func main() {
-	klog.Info("starting: %s", addonName)
-
 	kubeconfig, err := restclient.InClusterConfig()
 	if err != nil {
 		klog.ErrorS(err, "Unable to get in cluster kubeconfig")
-		os.Exit(1)
-	}
-	addonClient, err := addonv1alpha1client.NewForConfig(kubeconfig)
-	if err != nil {
-		klog.ErrorS(err, "unable to setup addon client")
 		os.Exit(1)
 	}
 	addonMgr, err := addonmanager.New(kubeconfig)
@@ -38,18 +27,18 @@ func main() {
 		klog.ErrorS(err, "unable to setup addon manager")
 		os.Exit(1)
 	}
-	err = addonMgr.AddAgent(&hub.OLMAgentManager{
-		AddonClient:  addonClient,
-		AddonName:    addonName,
-		OLMManifests: FS,
-	})
+	agent, err := hub.NewAgent(kubeconfig, FS)
+	if err != nil {
+		klog.ErrorS(err, "unable to build addon agent")
+		os.Exit(1)
+	}
+	err = addonMgr.AddAgent(agent)
 	if err != nil {
 		klog.ErrorS(err, "unable to add addon agent to manager")
 		os.Exit(1)
 	}
-
 	ctx := context.Background()
+	klog.Info("starting olm-addon")
 	go addonMgr.Start(ctx)
-
 	<-ctx.Done()
 }
